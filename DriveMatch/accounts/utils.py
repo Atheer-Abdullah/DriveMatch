9ad80fbf4cc39b2c8a6777265123f939f78d166b
Atheer Alharthi <atheerabdullahalharthi@gmail.com>
2026-05-06 14:51:05 +0300

    Update utils.py

--- DriveMatch/accounts/utils.py
+++ DriveMatch/accounts/utils.py
@@ -2,42 +2,32 @@ import random
 from django.utils import timezone
 from datetime import timedelta
 from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import EmailOTP
 
-
-
 def generate_otp():
     return str(random.randint(100000, 999999))
-
-
 
 def send_otp_email(email, code):
     send_mail(
         subject='DriveMatch — Email Verification Code',
         message=f'Your verification code is: {code}',
-        from_email=None,  
+        from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[email],
         fail_silently=False,
     )
 
-
-
 def create_otp(email, purpose):
     code = generate_otp()
-
     otp = EmailOTP.objects.create(
         email=email,
         otp_code=code,
         purpose=purpose,
         expires_at=timezone.now() + timedelta(minutes=10)
     )
-
     send_otp_email(email, code)
-
     return otp
-
-
 
 def verify_otp(email, code, purpose):
     try:
@@ -47,14 +37,10 @@ def verify_otp(email, code, purpose):
             purpose=purpose,
             is_used=False
         ).latest('created_at')
-
         if otp.expires_at < timezone.now():
             return False
-
         otp.is_used = True
         otp.save()
-
         return True
-
     except EmailOTP.DoesNotExist:
         return False
